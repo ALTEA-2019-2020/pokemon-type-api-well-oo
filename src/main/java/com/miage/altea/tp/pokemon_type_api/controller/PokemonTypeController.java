476cef7c76ec6d5e14e7f78773e6504d89c3a98d
well-oo <christopher.ducrocq.etu@univ-lille.fr>
2020-01-21 00:00:12 +0100

    Partie 8

---
+++ src/main/java/com/miage/altea/tp/pokemon_type_api/controller/PokemonTypeController.java
@@ -0,0 +1,33 @@
+package com.miage.altea.tp.pokemon_type_api.controller;
+
+import com.miage.altea.tp.pokemon_type_api.bo.PokemonType;
+import com.miage.altea.tp.pokemon_type_api.service.PokemonTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/pokemon-types")
+public class PokemonTypeController {
+
+    private PokemonTypeService pokemonTypeService;
+
+    @Autowired
+    public PokemonTypeController(PokemonTypeService pokemonTypeService) {
+        this.pokemonTypeService = pokemonTypeService;
+    }
+
+    @GetMapping("/{id}")
+    PokemonType getPokemonTypeFromId(@PathVariable int id){
+        return this.pokemonTypeService.getPokemonType(id);
+    }
+
+    @GetMapping("/")
+    public List<PokemonType> getAllPokemonTypes() {
+        return this.pokemonTypeService.getAllPokemonTypes();
+    }
+}

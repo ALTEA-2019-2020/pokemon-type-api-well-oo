fd739b534e1117450fa08b01f9a0cc4b98919465
Ducrocq Christopher <christopher.ducrocq.etu@univ-lille.fr>
2020-01-20 13:06:42 +0100

    Partie 5

---
+++ src/main/java/com/miage/altea/tp/pokemon_type_api/repository/PokemonTypeRepositoryImpl.java
@@ -0,0 +1,43 @@
+package com.miage.altea.tp.pokemon_type_api.repository;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.miage.altea.tp.pokemon_type_api.bo.PokemonType;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class PokemonTypeRepositoryImpl implements PokemonTypeRepository {
+
+    private List<PokemonType> pokemons;
+
+    public PokemonTypeRepositoryImpl() {
+        try {
+            var pokemonsStream = this.getClass().getResourceAsStream("/pokemons.json");
+
+            var objectMapper = new ObjectMapper();
+            var pokemonsArray = objectMapper.readValue(pokemonsStream, PokemonType[].class);
+            this.pokemons = Arrays.asList(pokemonsArray);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public PokemonType findPokemonTypeById(int id) {
+        System.out.println("Loading Pokemon information for Pokemon id " + id);
+        return this.pokemons.stream().filter(e-> e.getId() == id).findFirst().get();
+    }
+
+    @Override
+    public PokemonType findPokemonTypeByName(String name) {
+        System.out.println("Loading Pokemon information for Pokemon name " + name);
+        return this.pokemons.stream().filter(e-> e.getName().equals(name)).findFirst().get();
+    }
+
+    @Override
+    public List<PokemonType> findAllPokemonType() {
+        return this.pokemons;
+    }
+
+}

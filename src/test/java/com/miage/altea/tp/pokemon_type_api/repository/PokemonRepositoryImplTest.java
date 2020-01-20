f7c07965d76f9a039c43c21f11327c770c52c2ec
well-oo <christopher.ducrocq.etu@univ-lille.fr>
2020-01-20 22:55:44 +0100

    Partie 6

--- src/test/java/com/miage/altea/tp/pokemon_type_api/repository/PokemonRepositoryImplTest.java
+++ src/test/java/com/miage/altea/tp/pokemon_type_api/repository/PokemonRepositoryImplTest.java
@@ -1,6 +1,7 @@
 package com.miage.altea.tp.pokemon_type_api.repository;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -45,5 +46,17 @@ public class PokemonRepositoryImplTest {
         var pokemons = repository.findAllPokemonType();
         assertNotNull(pokemons);
         assertEquals(151, pokemons.size());
+    }
+
+    @Test
+    void applicationContext_shouldLoadPokemonRepository(){
+
+        var context = new AnnotationConfigApplicationContext("com.miage.altea.tp.pokemon_type_api.repository");
+        var repoByName = context.getBean("pokemonTypeRepositoryImpl");
+        var repoByClass = context.getBean(PokemonTypeRepository.class);
+
+        assertEquals(repoByName, repoByClass);
+        assertNotNull(repoByName);
+        assertNotNull(repoByClass);
     }
 }

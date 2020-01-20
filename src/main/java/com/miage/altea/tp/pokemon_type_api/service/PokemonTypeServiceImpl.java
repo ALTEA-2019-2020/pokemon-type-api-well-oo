f5f3832e2c2eabc44037a8a665e1352ffbe7b0be
well-oo <christopher.ducrocq.etu@univ-lille.fr>
2020-01-20 23:16:45 +0100

    Service + modification du pom

---
+++ src/main/java/com/miage/altea/tp/pokemon_type_api/service/PokemonTypeServiceImpl.java
@@ -0,0 +1,29 @@
+package com.miage.altea.tp.pokemon_type_api.service;
+
+import com.miage.altea.tp.pokemon_type_api.bo.PokemonType;
+import com.miage.altea.tp.pokemon_type_api.repository.PokemonTypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PokemonTypeServiceImpl implements PokemonTypeService {
+
+    public PokemonTypeRepository pokemonTypeRepository;
+
+    @Autowired
+    public PokemonTypeServiceImpl(PokemonTypeRepository pokemonTypeRepository){
+        this.pokemonTypeRepository = pokemonTypeRepository;
+    }
+
+    @Override
+    public PokemonType getPokemonType(int id) {
+        return this.pokemonTypeRepository.findPokemonTypeById(id);
+    }
+
+    @Override
+    public List<PokemonType> getAllPokemonTypes(){
+        return this.pokemonTypeRepository.findAllPokemonType();
+    }
+}
